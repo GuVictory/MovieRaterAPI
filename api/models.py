a6b13cb0918d9ee9b5f3961f759f0ae42d683df4
GuVictory <gubanova.vi10@gmail.com>
2020-09-20 15:06:02 +0300

    models of movie and rating added

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title = models.CharField(max_length=32)
+        self.description = models.TextField(max_length=380)
+
+
+class Rating(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(10)])
+
+    class Meta:
+        unique_together = (('user', 'movie'),)
+        index_together = (('user', 'movie'),)
